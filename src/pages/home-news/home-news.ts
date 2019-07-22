79abca65eb57e809ac1e89791c658aff54144299
nvdinh185 <nvdinh0905671805@gmail.com>
2019-07-22 16:50:54 +0700

    first commit

--- src/pages/home-news/home-news.ts
+++ src/pages/home-news/home-news.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
 import { Events, ModalController } from 'ionic-angular';
 import { ApiAuthService } from '../../services/apiAuthService';
-import { DynamicCardSocialPage } from '../dynamic-card-social/dynamic-card-social';
 import { ApiContactService } from '../../services/apiContactService';
 import { DynamicPostPage } from '../dynamic-post/dynamic-post';
 
@@ -14,11 +13,7 @@ export class HomeNewsPage {
   //server = ApiStorageService.newsServer;
   server = "http://localhost:9238/site-manager/news"
   userInfo: any;
-  curPageIndex = 0;
-  lastPageIndex = 0;
-  maxOnePage = 3;
   contacts = {}
-  isShow = false;
 
   constructor(private events: Events
     , public modalCtrl: ModalController
@@ -34,7 +29,7 @@ export class HomeNewsPage {
     }, 2000);
     this.refreshNews();
     this.events.subscribe('postok', () => {
-      this.getHomeNews(true);
+      this.getHomeNews();
     });
   }
 
@@ -52,50 +47,26 @@ export class HomeNewsPage {
     //lay cac danh ba public
     this.contacts = this.apiContact.getUniqueContacts();
     //neu chua dang nhap thi lay cac tin cua user public
-    this.getHomeNews(true);
+    this.getHomeNews();
   }
 
-  getHomeNews(reNews?: boolean) {
-    //console.log("456", this.contacts)
-    this.dynamicCards.title = "Đây là trang tin của " + (this.userInfo ? this.userInfo.username : "Public")
-    if (reNews) {
-      this.lastPageIndex = this.curPageIndex > 0 ? this.curPageIndex : this.lastPageIndex;
-      this.curPageIndex = 0;
-    } else {
-      this.lastPageIndex = this.curPageIndex > this.lastPageIndex ? this.curPageIndex : this.lastPageIndex;
-      this.curPageIndex = this.curPageIndex < this.lastPageIndex ? this.lastPageIndex : this.curPageIndex;
-    }
+  getHomeNews() {
     this.getJsonPostNews()
       .then(data => {
-        if (reNews) {
-          //console.log("new: ", data)
-          let isHaveNew = 0;
-          data.reverse().forEach((el, idx) => {
-            let index = this.dynamicCards.items
-              .findIndex(x => x.group_id === el.group_id);
-            if (index < 0) {
-              this.dynamicCards.items.unshift(el);
-              isHaveNew++;
-            }
-          })
-          if (isHaveNew >= 1 && this.curPageIndex < this.lastPageIndex) this.curPageIndex = this.lastPageIndex + 1
-        } else {
-          //console.log("data: ", data)
-          data.forEach((el, idx) => {
-            let index = this.dynamicCards.items
-              .findIndex(x => x.group_id === el.group_id);
-            if (index < 0) {
-              this.dynamicCards.items.push(el);
-            }
-          })
-        }
+        data.reverse().forEach((el, idx) => {
+          let index = this.dynamicCards.items
+            .findIndex(x => x.group_id === el.group_id);
+          if (index < 0) {
+            this.dynamicCards.items.unshift(el);
+          }
+        })
       })
       .catch(err => console.log(err))
   }
 
   getJsonPostNews() {
-    let offset = this.curPageIndex * this.maxOnePage;
-    let limit = this.maxOnePage;
+    let offset = 0
+    let limit = 4
     let follows = [];
     for (let key in this.contacts) {
       follows.push(key);
@@ -106,7 +77,6 @@ export class HomeNewsPage {
       offset: offset,
       follows: follows
     }
-    //console.log("json_data", json_data)
     return this.auth.postDynamicForm(this.server + "/get-news", json_data, true)
       .then(data => {
         console.log("789", data)
@@ -114,7 +84,7 @@ export class HomeNewsPage {
         data.forEach(el => {
           el.actions = JSON.parse(el.actions)
           el.results = JSON.parse(el.results)
-          if(el.medias) {
+          if (el.medias) {
             el.results.likes = JSON.parse(el.medias.likes)
             el.results.comments = JSON.parse(el.medias.comments)
             el.results.shares = JSON.parse(el.medias.shares)
@@ -127,24 +97,9 @@ export class HomeNewsPage {
           }
           items.push(el);
         });
-        if (items.length > 0) this.curPageIndex++;
         return items;
       })
       .catch(err => { return [] })
-  }
-
-  doInfinite(ev) {
-    this.getHomeNews();
-    setTimeout(() => {
-      ev.complete();
-    }, 500);
-  }
-
-  doRefresh(ev) {
-    this.getHomeNews(true);
-    setTimeout(() => {
-      ev.complete();
-    }, 500);
   }
 
   onClickHeader(btn) {
@@ -161,39 +116,6 @@ export class HomeNewsPage {
         }
       })
     }
-  }
-
-  onClickMedia(number, it) {
-    let dynamicCardsOrigin: any = {
-      title: it.user
-      , buttons: [
-        { color: "danger", icon: "close", next: "CLOSE" }
-      ]
-      , items: [
-        {
-          short_detail: {
-            avatar: this.userInfo ? this.userInfo.data.image : ""
-            , h1: this.userInfo ? this.userInfo.data.fullname : ""
-            , p: it.content
-            , note: it.time
-            , action: { color: "primary", icon: "more", next: "MORE" }
-          }
-          , content: {
-            title: it.title
-            , paragraphs: [
-              {
-                //h2: "Chốn yên bình"
-                //, p: "Là nơi bình yên nhất. Bạn có thể dạo bước trên con đường rợp bóng mát thanh bình đến lạ"
-                medias: it.medias
-              }
-            ]
-            , note: "Nguyễn Văn Định 2019"
-          }
-          , actions: it.actions
-        }
-      ]
-    };
-    this.openModal(DynamicCardSocialPage, { form: dynamicCardsOrigin })
   }
 
   openModal(form, data?: any) {
